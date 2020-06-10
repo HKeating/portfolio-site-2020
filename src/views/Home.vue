5acabde4d4121ec5f647f9ac13015eeb884a2964
HKeating <horacekeating@hotmail.co.uk>
2020-06-10 19:27:02 +0100

    Bring in assets from old site. Add fonts.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Site logo" src="../assets/images/site-icon.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
