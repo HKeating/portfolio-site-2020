8f3b3fc82355647b7d062f52751f2b19c4ae4241
HKeating <horacekeating@hotmail.co.uk>
2020-06-11 18:07:14 +0100

    Lint and add simple route transition

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,26 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  computed: {
+    transitionName() {
+      if (this.$route.name === 'home') return 'slide-left';
+      else return 'slide-right';
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -28,4 +45,26 @@
     }
   }
 }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 200ms ease-in-out;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+// .slide-left-enter-active, .slide-left-leave-active {
+//   transition: transform 300ms ease-in-out;
+// }
+// .slide-left-enter, .slide-left-leave-to {
+//   transform: translateX(-30vw);
+// }
+
+// .slide-right-enter-active, .slide-right-leave-active {
+//   transition: transform 300ms ease-in-out;
+// }
+// .slide-right-enter, .slide-right-leave-to {
+//   transform: translateX(30vw);
+// }
 </style>
