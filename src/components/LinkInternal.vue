acdf02a8c982d5228a9e8cbb814e7db0d6c5bd12
HKeating <horacekeating@hotmail.co.uk>
2020-06-27 18:00:03 +0100

    Add link-internal component

---
+++ src/components/LinkInternal.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link class="text-blue-100 hover:underline" :to="routeName">
+    <slot />
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'LinkExternal',
+  props: {
+    routeName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="postcss">
+.bg-red-75 > p > a,
+.bg-blue-75 > p > a,
+.bg-red-50 > p > a,
+.bg-blue-50 > p > a {
+  @apply text-yellow-100;
+}
+</style>
