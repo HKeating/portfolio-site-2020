1ce51b5eb422d71030cd70fc08ee3535577add84
HKeating <horacekeating@hotmail.co.uk>
2020-06-27 12:46:58 +0100

    Add link-external component to standardise a tag markup

---
+++ src/components/LinkExternal.vue
@@ -0,0 +1,22 @@
+<template>
+  <a
+    class="text-blue-100 hover:underline"
+    target="_blank"
+    rel="noopener noreferrer"
+    :href="linkUrl"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'LinkExternal',
+  props: {
+    linkUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
