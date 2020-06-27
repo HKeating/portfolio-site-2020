b1e39dd295da0a56c6c2d4364d053f2f61374f9c
HKeating <horacekeating@hotmail.co.uk>
2020-06-27 17:52:11 +0100

    Add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
 const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss, autoprefixer, postcssImport],
 };
