17106b3028ecd4501809b4a420214d036cf7951b
HKeating <horacekeating@hotmail.co.uk>
2020-06-30 22:14:33 +0100

    Add copy for PV app on current work page

--- src/views/CurrentWork.vue
+++ src/views/CurrentWork.vue
@@ -22,7 +22,7 @@
       <img
         class="object-cover w-full h-full"
         src="@/assets/images/pa-platform.jpg"
-        alt="Podcast matching app"
+        alt="Leadership training platform"
       />
     </div>
     <div
@@ -90,17 +90,6 @@
         developer to add multi-language support. This involved updating the CMS
         to include separate versions of each page for each supported language.
       </p>
-      <!-- <p class="text-base">
-        <link-external
-          :linkUrl="
-            'https://www.paconsulting.com/insights/2018/leadership-essentials/'
-          "
-          >learning platforms</link-external
-        >, websites, native apps, and
-        <link-external :linkUrl="'https://pulsevisual.com/'"
-          >SaaS systems</link-external
-        >
-      </p> -->
     </div>
     <div
       class="m-2 p-2 border-t-2 border-r-2 border-black border-opacity-75 col-start-1 col-span-2 sm:row-start-6 sm:col-start-1 md:col-start-2 md:col-span-3 md:row-start-6 lg:row-start-5 lg:col-start-4 lg:col-span-6"
@@ -147,86 +136,47 @@
     <div
       class="m-2 p-2 border-t-2 border-l-2 border-black border-opacity-75 col-start-1 col-span-2 sm:col-start-1 md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-6"
     >
-      <h2 class="text-xl">SaaS Systems</h2>
+      <h2 class="text-xl">SaaS Succession Planning</h2>
       <p class="text-base">
-        Blah Blah blah succession planning SaaS pulsevisual etc.
-      </p>
-    </div>
-    <!-- <div
-      class="m-2 p-2 border-b-2 border-r-2 border-black border-opacity-75 col-span-1 col-start-2 row-start-5 md:col-start-3 md:col-span-2 lg:col-start-6 lg:col-span-4"
-    >
-      <p class="text-base">
-        For my first project I built
-        <link-external :linkUrl="'https://flappy-sausage.herokuapp.com'"
-          >a game</link-external
-        >
-        similar to the popular Flappy Bird.
-      </p>
-      <p class="text-base">- Built with JavaScript, jQuery, HTML and CSS.</p>
-    </div>
-    <div
-      class="m-2 p-2 border-t-2 border-r-2 border-black border-opacity-75 col-span-2 col-start-1 row-start-6 md:col-start-2 md:col-span-3 lg:col-start-4 lg:col-span-6"
-    >
-      <p class="text-base">
-        The second project was my first full-stack web app.
-        <link-external :linkUrl="'https://pod-me.herokuapp.com/'"
-          >PodMe</link-external
-        >
-        allows a user to find podcast episodes that match the duration of a
-        given journey by making calls to the Google Maps and iTunes APIs and
-        algorithmically sorting the returned episodes.
-      </p>
-      <p class="text-base">
-        - Built with Express.js, Node.js, jQuery, MongoDB, bcrypt, slick.js and
-        Materialize.
+        For the past two years I have led front-end development for a
+        <link-external :linkUrl="'https://pulsevisual.com/'"
+          >succession planning SaaS system</link-external
+        >. The service comprises a native iOS and Android app, built with
+        Cordova, and a web portal, built with Vue.js.
       </p>
     </div>
     <div
-      class="m-2 p-2 border-b-2 border-l-2 border-black border-opacity-75 col-span-2 col-start-1 row-start-7 sm:col-span-1 md:col-start-1 md:col-span-2 lg:col-start-4 lg:col-span-5"
+      class="m-2 p-2 border-b-2 border-l-2 md:border-t-2 md:border-b-0 border-black border-opacity-75 col-start-1 col-span-2 md:col-start-1 md:col-span-2 md:row-span-1 lg:col-start-3 lg:col-span-6"
     >
+      <h2 class="text-lg">Highly Configurable</h2>
       <p class="text-base">
-        Project three was a MEAN-stack application.
-        <link-external :linkUrl="'https://feelgood-hkeating.herokuapp.com/'"
-          >The site</link-external
-        >
-        allows users to post jobs or tasks that they need help with, other users
-        can then apply to help out with those tasks in exchange for the original
-        poster making a donation to charity.
-      </p>
-      <p class="text-base">
-        - Built with MongoDB, Express.js, AngularJS and Node.js, plus AngularJS
-        Material, and Tachyons.
+        As part of the process of white-labelling the app I designed a system to
+        configure each app instance according to the client requirements and
+        data schema. This increased the ability of our client to customise the
+        look and feel, as well as the data insights, of each app instance.
       </p>
     </div>
     <div
-      class="m-2 col-span-2 row-span-1 col-start-1 sm:col-span-1 sm:col-start-2 sm:row-start-7 md:col-start-3 row-start-8 md:col-span-2 lg:col-start-9 lg:col-span-4 border-r-2 border-t-2 border-black border-opacity-75 p-2"
+      class="m-2 col-span-2 row-span-1 col-start-1 sm:col-start-1 md:col-start-3 md:col-span-2 lg:col-start-9 lg:col-span-4 border-r-2 border-t-2 border-black border-opacity-75 p-2"
     >
       <img
         class="object-cover w-full h-full"
         src="@/assets/images/feel-good.jpg"
-        alt="JavaScript game screenshot"
+        alt="Pulse Visual app"
       />
     </div>
     <div
-      class="m-2 p-2 border-b-2 border-r-2 border-black border-opacity-75 col-span-2 col-start-1 row-start-9 md:col-start-1 md:col-span-4 lg:col-start-4 lg:col-span-6"
+      class="m-2 p-2 border-b-2 border-r-2 border-black border-opacity-75 col-start-1 col-span-2 md:col-start-1 md:col-span-4 lg:col-start-3 lg:col-span-8"
     >
+      <h2 class="text-lg">Deployment and Distribution</h2>
       <p class="text-base">
-        <link-external :linkUrl="'https://fck-it-shp-it.herokuapp.com/'"
-          >My final project</link-external
-        >
-        was a collaboration with two other WDI students. We built a
-        project-management tool specifically for developers.
+        I wrote build scripts that allowed us to abstract platform-specific
+        config, such as bundle ID, build number and display name into separate
+        files, rather than maintaining distinct git branches to hold config for
+        each app version. This streamlined the build and release process for the
+        apps, saving time and effort for myself and colleagues.
       </p>
-      <p class="text-base">
-        It allows the user to clearly visualise how long they have left to hit
-        their next deadline, milestone or sprint target while closely tracking
-        project activity so you can see who has been doing what on your project.
-      </p>
-      <p class="text-base">
-        - Built with a Ruby on Rails back-end and AngularJS front-end. We also
-        used Angular-gridster, Angular Dragdrop and Angular-avatar.
-      </p>
-    </div> -->
+    </div>
   </div>
 </template>
 
